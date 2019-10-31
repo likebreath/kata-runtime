149a7b4c229d31f16f380d4842cdcd78ea0bc5ad
Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>
2019-10-31 16:09:07 +0000

    ch: implement boot
    boot VM
    
    Signed-off-by: Jose Carlos Venegas Munoz <jose.carlos.venegas.munoz@intel.com>

--- virtcontainers/ch.go
+++ virtcontainers/ch.go
@@ -1,10 +1,21 @@
 package virtcontainers
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
 
+	"github.com/kata-containers/runtime/virtcontainers/device/config"
 	persistapi "github.com/kata-containers/runtime/virtcontainers/persist/api"
+	chclient "github.com/kata-containers/runtime/virtcontainers/pkg/cloud-hypervisor/client"
 	"github.com/kata-containers/runtime/virtcontainers/store"
 	"github.com/kata-containers/runtime/virtcontainers/types"
 	"github.com/kata-containers/runtime/virtcontainers/utils"
@@ -15,21 +26,23 @@ const (
 	apiSocket = "api.sock"
 )
 
-// QemuState keeps Qemu's state
 type cloudHypervisorState struct {
 	apiSocket string
+	pid       int
 }
 
 type cloudHypervisor struct {
-	id     string
-	store  *store.VCStore
-	config HypervisorConfig
-	state  cloudHypervisorState
+	id        string
+	store     *store.VCStore
+	config    HypervisorConfig
+	state     cloudHypervisorState
+	ApiClient *chclient.DefaultApiService
+	vmconfig  chclient.VmConfig
 }
 
 // INIT hypervisor interface implementation functions
-func (c *cloudHypervisor) createSandbox(ctx context.Context, id string, networkNS NetworkNamespace, hypervisorConfig *HypervisorConfig, store *store.VCStore) error {
-	c.Logger().Debug("Creating Sandbox for dor cloud-hypervisor")
+func (c *cloudHypervisor) createSandbox(ctx context.Context, id string, networkNS NetworkNamespace, hypervisorConfig *HypervisorConfig, vcstore *store.VCStore) error {
+	c.Logger().Debug("Creating Sandbox for for cloud-hypervisor")
 
 	err := hypervisorConfig.valid()
 	if err != nil {
@@ -37,27 +50,35 @@ func (c *cloudHypervisor) createSandbox(ctx context.Context, id string, networkN
 	}
 
 	c.id = id
-	c.store = store
+	c.store = vcstore
 	c.config = *hypervisorConfig
-	c.state.apiSocket, err = c.generateAPISocket()
+
+	vmPath := filepath.Join(store.RunVMStoragePath(), c.id)
+	if err := os.MkdirAll(vmPath, store.DirMode); err != nil {
+		return err
+	}
+	apiSocket, err := utils.BuildSocketPath(store.RunVMStoragePath(), c.id, apiSocket)
 	if err != nil {
 		return err
 	}
+	c.state.apiSocket = apiSocket
 
 	return nil
 }
 
 func (c *cloudHypervisor) startSandbox(timeout int) error {
-	var args []string
-	var cmd *exec.Cmd
-	args = []string{"--api-socket", c.state.apiSocket}
-	cmd = exec.Command(c.config.HypervisorPath, args...)
-	c.Logger().WithField("hypervisor cmd", cmd.Path).Debug()
-	c.Logger().WithField("hypervisor args", cmd.Args).Debug()
-	if err := cmd.Start(); err != nil {
-		c.Logger().WithField("Error starting hypervisor", err).Debug()
+	if err := c.Start(); err != nil {
 		return err
 	}
+
+	if err := c.waitAPIServer(timeout); err != nil {
+		return err
+	}
+
+	if err := c.bootVM(timeout); err != nil {
+		return err
+	}
+
 	return nil
 }
 func (c *cloudHypervisor) stopSandbox() error {
@@ -77,6 +98,33 @@ func (c *cloudHypervisor) resumeSandbox() error {
 }
 
 func (c *cloudHypervisor) addDevice(devInfo interface{}, devType deviceType) error {
+
+	switch v := devInfo.(type) {
+	case types.Volume:
+		if c.config.SharedFS == config.VirtioFS {
+			return fmt.Errorf("VirtioFS not implemented")
+		} else {
+			return fmt.Errorf("shared fs method not supported")
+		}
+	case types.Socket:
+
+		return fmt.Errorf("Not implemented Socket")
+	case types.VSock:
+		return fmt.Errorf("Not implemented VSocket")
+	case types.HybridVSock:
+		//TODO: fix API to use int64
+		c.vmconfig.Vsock = []chclient.VsockConfig{{Cid: int32(defaultGuestVSockCID), Sock: v.UdsPath}}
+	case Endpoint:
+		return fmt.Errorf("Not implemented Endpoint")
+	case config.BlockDrive:
+		return fmt.Errorf("Not implemented BlockDrive")
+	case config.VhostUserDeviceAttrs:
+		return fmt.Errorf("Not implemented VhostUserDeviceAttrs")
+	case config.VFIODev:
+		return fmt.Errorf("Not implemented VFIODev")
+	default:
+		return fmt.Errorf("Not implemented support for %s", v)
+	}
 	return nil
 }
 
@@ -150,10 +198,8 @@ func (c *cloudHypervisor) generateSocket(id string, useVsock bool) (interface{},
 
 // END hypervisor interface implementation functions
 
-func (c *cloudHypervisor) generateAPISocket() (string, error) {
-	return utils.BuildSocketPath(store.RunVMStoragePath(), c.id, apiSocket)
-}
 func (c *cloudHypervisor) storeState() error {
+	//TODO: should be mixed with save method?
 	if c.store != nil {
 		if err := c.store.Store(store.Hypervisor, c.state); err != nil {
 			return err
@@ -164,4 +210,194 @@ func (c *cloudHypervisor) storeState() error {
 
 func (c *cloudHypervisor) Logger() *logrus.Entry {
 	return virtLog.WithField("subsystem", "cloud-hypervisor")
+}
+
+func (c *cloudHypervisor) isRunning() bool {
+	// Todo if we launched and have not wait for it this will return 0 but the process
+	// is zombie, need extra check to no is running ?
+	if err := syscall.Kill(c.state.pid, syscall.Signal(0)); err != nil {
+		return false
+	}
+	return true
+}
+
+func (c *cloudHypervisor) Start() error {
+	var args []string
+	var cmd *exec.Cmd
+	args = []string{"--api-socket", c.state.apiSocket}
+	cmd = exec.Command(c.config.HypervisorPath, args...)
+
+	cmdOutput := &bytes.Buffer{}
+	cmd.Stdout = cmdOutput
+
+	c.Logger().WithField("hypervisor cmd", cmd.Path).Debug()
+	c.Logger().WithField("hypervisor args", cmd.Args).Debug()
+
+	if err := cmd.Start(); err != nil {
+		c.Logger().Errorf("%s", string(cmdOutput.Bytes()))
+		c.Logger().WithField("Error starting hypervisor", err).Error()
+		return err
+	}
+	c.state.pid = cmd.Process.Pid
+
+	return nil
+}
+
+func (c *cloudHypervisor) waitAPIServer(timeout int) error {
+	// TODO: Add tracing
+
+	ctx := context.Background()
+	if timeout < 0 {
+		return fmt.Errorf("Invalid timeout %ds", timeout)
+	}
+
+	timeStart := time.Now()
+	cl := c.client()
+	for {
+
+		info, res, err := cl.VmmInfoGet(ctx)
+		if err == nil {
+			c.Logger().Debug("TODO Vmm version ", info.Version)
+			fmt.Println("check reponse", info, res)
+			return nil
+		}
+
+		if int(time.Since(timeStart).Seconds()) > timeout {
+			return fmt.Errorf("Failed to connect to API (timeout %ds): %s", timeout, openApiClientError(err))
+		}
+
+		time.Sleep(time.Duration(10) * time.Millisecond)
+	}
+}
+
+func openApiClientError(err error) error {
+
+	if err == nil {
+		return nil
+	}
+
+	reason := ""
+	if apierr, ok := err.(chclient.GenericOpenAPIError); ok {
+		reason = string(apierr.Body())
+	}
+
+	return fmt.Errorf("error: %v reason: %s", err, reason)
+}
+
+func (c *cloudHypervisor) bootVM(timeout int) error {
+	// TODO: Add tracing
+
+	if timeout < 0 {
+		return fmt.Errorf("Invalid timeout %ds", timeout)
+	}
+
+	ctx := context.Background()
+
+	kernelPath, err := c.config.KernelAssetPath()
+	if err != nil {
+		return err
+	}
+
+	imagePath, err := c.config.ImageAssetPath()
+	if err != nil {
+		return err
+	}
+
+	// Get Kernel params
+	strParams := SerializeParams(kernelParams, "=")
+	kernel_cmdline := strings.Join(strParams, " ")
+
+	// Kernel config
+	c.vmconfig.Kernel.Path = kernelPath
+	c.vmconfig.Cmdline.Args = kernel_cmdline
+
+	// Disk config
+	c.vmconfig.Disks = []chclient.DiskConfig{{Path: imagePath}}
+
+	// Initial VM Reesources
+	c.vmconfig.Cpus.CpuCount = int32(c.config.NumVCPUs)
+	// TODO
+	/// vmconfig.Memory.Size = c.config.MemorySize
+	// api wrapper convert to u32, need to fix with
+	// type: integer
+	//format: int64
+	c.vmconfig.Memory.Size = 536870912
+
+	c.vmconfig.Serial = chclient.ConsoleConfig{Mode: "Tty", File: ""}
+	c.vmconfig.Console = chclient.ConsoleConfig{Mode: "Off", File: ""}
+
+	// No default value and goes emtpy
+	c.vmconfig.Rng.Src = "/dev/urandom"
+
+	c.Logger().Debugf("VMconfig %#v", c.vmconfig)
+
+	cl := c.client()
+	_, err = cl.CreateVM(ctx, c.vmconfig)
+
+	if err != nil {
+		return err
+	}
+
+	info, _, err := cl.VmInfoGet(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	c.Logger().Debugf("VM state after create: %#v", info)
+
+	if info.State != "Created" {
+		return fmt.Errorf("VM state is not created after create")
+	}
+
+	c.Logger().Debug("Booting VM")
+	_, err = cl.BootVM(ctx)
+
+	if err != nil {
+		return openApiClientError(err)
+	}
+
+	info, _, err = cl.VmInfoGet(ctx)
+
+	if err != nil {
+		return openApiClientError(err)
+	}
+
+	c.Logger().Debugf("VM state after boot: %#v", info)
+
+	if info.State != "Running" {
+		return fmt.Errorf("VM state is not created after create")
+	}
+
+	return nil
+}
+
+func (c *cloudHypervisor) client() *chclient.DefaultApiService {
+	if c.ApiClient == nil {
+		c.ApiClient = c.newAPIClient()
+	}
+
+	return c.ApiClient
+}
+
+func (c *cloudHypervisor) newAPIClient() *chclient.DefaultApiService {
+
+	cfg := chclient.NewConfiguration()
+
+	socketTransport := &http.Transport{
+		DialContext: func(ctx context.Context, network, path string) (net.Conn, error) {
+			addr, err := net.ResolveUnixAddr("unix", c.state.apiSocket)
+			if err != nil {
+				return nil, err
+
+			}
+
+			return net.DialUnix("unix", nil, addr)
+		},
+	}
+
+	cfg.HTTPClient = http.DefaultClient
+	cfg.HTTPClient.Transport = socketTransport
+
+	return chclient.NewAPIClient(cfg).DefaultApi
 }
